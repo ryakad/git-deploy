6e6c239cef01ca58144658c39c8cda4c367ebb55
Ryan Kadwell <ryan@riaka.ca>
2013-11-04 08:18:36 -0700

    Added documentation and readme

---
+++ auto-deploy.py
@@ -0,0 +1,7 @@
+# Riaka Auto Deploy
+#
+# Copyright (c) Ryan Kadwell <ryan@riaka.ca>
+#
+# Simple Script to be run in the git post-receive hook to update the remotes
+# on projects.
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+Git Auto Deploy
+===============
+
+Simple auto deploy script for deploying a project from git's post-receive
+hook.
+
